9f2cd3752357a1f8b3dc2e3cc28195498eb3e53f
= <=>
2021-04-14 22:47:58 -0400

    Problema8

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
-function palindromeChecker(str) {
-    const newStr = str.replace(/[\W_]/g, "").toLowerCase()
-    const strReversed = newStr.split("").reverse().join("")
-  
-    return newStr === strReversed ? "true" : "false"
-  }
-  
-  console.log(palindromeChecker("Ali tomo tilathis is a sample string")) 
-  console.log(palindromeChecker("a")) 
-  console.log(palindromeChecker("bangnab")) 
+const elementos = 14;
+const primos = [];
+let iteraciones = 0;
 
+for(let candidato = 2; candidato <= elementos; candidato++) {
+    let primo = true;
+    for(let divisor=2; divisor < candidato; divisor++) {
+        iteraciones++;
+        if(candidato%divisor === 0) {
+            primo = !primo;
+            break;
+        }
+    }
+    if(primo) primos.push(candidato);
+}
+
+console.log(`iteraciones: ${iteraciones}`);
+console.log(primos);
